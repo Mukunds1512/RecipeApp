4f14be60e77677dbe1c5281f547d90b58223d5e1
Mukunds1512 <mukunds1512@gmail.com>
2024-08-06 14:26:47 -0400

    RecipeListViewController

---
+++ RecipeApp/RecipeListViewController.swift
@@ -0,0 +1,39 @@
+//
+//  RecipeListViewController.swift
+//  RecipeApp
+//
+//  Created by Mukund Sharma on 8/6/24.
+//
+
+import Foundation
+import UIKit
+
+class RecipeListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    private let tableView = UITableView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Recipes"
+        setupTableView()
+    }
+
+    private func setupTableView() {
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "RecipeCell")
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recipes.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RecipeCell", for: indexPath)
+        cell.textLabel?.text = recipes[indexPath.row].name
+        return cell
+    }
+
+   
+}
--- RecipeApp/SceneDelegate.swift
+++ RecipeApp/SceneDelegate.swift
@@ -19,7 +19,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController  = ViewController()
+        let navigationController = UINavigationController(rootViewController:   RecipeListViewController())
+        window?.rootViewController  = navigationController
         window?.makeKeyAndVisible()
     }
 
