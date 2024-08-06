75f4e15323d93d67f7a29594394865f0802ad9fe
Mukunds1512 <mukunds1512@gmail.com>
2024-08-06 12:17:30 -0400

    Initial commit

---
+++ RecipeApp/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  RecipeApp
+//
+//  Created by Mukund Sharma on 8/6/24.
+//
+
+import Foundation
+
+struct Recipe {
+    let name: String
+    let image: String
+    let details: String
+}
+
---
+++ RecipeApp/RecipeData.swift
@@ -0,0 +1,14 @@
+//
+//  RecipeData.swift
+//  RecipeApp
+//
+//  Created by Mukund Sharma on 8/6/24.
+//
+
+import Foundation
+
+let recipes: [Recipe] = [
+    Recipe(name: "Spaghetti Carbonara", image: "carbonara", details: "Ingredients:\n- Spaghetti\n- Eggs\n- Cheese\n- Bacon\n- Pepper"),
+    Recipe(name: "Chicken Curry", image: "chicken_curry", details: "Ingredients:\n- Chicken\n- Curry Powder\n- Coconut Milk\n- Onion\n- Garlic"),
+    Recipe(name: "Tacos", image: "tacos", details: "Ingredients:\n- Tortillas\n- Beef\n- Cheese\n- Lettuce\n- Tomato")]
+    
